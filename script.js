0022e8db61920dd146a1c942a032d988d6c2a43f
ArielPetter <arielputzpetter@gmail.com>
2024-03-01 19:11:36 -0300

    startingJs

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+const convertButton = document.querySelector(".main-button")
 
+function convertValues(){
+    const inputValue = document.querySelector(".main-input").value 
+    const valueDolar = 4.95
 
+    const convertedValue = inputValue / valueDolar
+    console.log(convertedValue)
+}
 
-  
+convertButton.addEventListener("click", convertValues)
