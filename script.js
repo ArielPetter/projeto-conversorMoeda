7761e425a82080438996c6d099cc3df2c385a35f
ArielPetter <arielputzpetter@gmail.com>
2024-03-02 11:05:54 -0300

    melhor-html-css-js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const convertButton = document.querySelector(".main-button")
 
 function convertValues(){
-    const inputValue = document.querySelector(".main-input").value 
+    const inputValue = document.querySelector(".main-input").value // valor input
     const valueDolar = 4.95
+    const convertedValue = inputValue / valueDolar // valor do input / pelo dólar
+    const valueToConvert = document.querySelector(".valor-converter") // valor a converter 
+    const valueToConverted = document.querySelector(".valor-convertido") // valor convertido
 
-    const convertedValue = inputValue / valueDolar
-    console.log(convertedValue)
+    valueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputValue)
+    valueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style:"currency",
+        currency:"USD"
+    }).format(convertedValue)
 }
 
 convertButton.addEventListener("click", convertValues)
+
+
+
