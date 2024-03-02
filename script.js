24e8eb7ef837d22d7075131859311a26ca7e0dc5
ArielPetter <arielputzpetter@gmail.com>
2024-03-02 12:09:58 -0300

    corrections

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-const convertButton = document.querySelector(".main-button")
+const convertButton = document.querySelector(".main-button");
+const select = document.querySelector(".select-convertido");
 
-function convertValues(){
-    const inputValue = document.querySelector(".main-input").value // valor input
-    const valueDolar = 4.95
-    const convertedValue = inputValue / valueDolar // valor do input / pelo dólar
-    const valueToConvert = document.querySelector(".valor-converter") // valor a converter 
-    const valueToConverted = document.querySelector(".valor-convertido") // valor convertido
+function convertValues() {
+  const inputValue = document.querySelector(".main-input").value; // valor input
+  const valueDolar = 4.95;
+  const valueEuro = 5.38;
+  const valueToConvert = document.querySelector(".valor-converter"); // valor a converter
+  const valueToConverted = document.querySelector(".valor-convertido"); // valor convertido
 
-    valueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL"
-    }).format(inputValue)
+  if (select.value == "dolar") {
+    // quando dolar selecionado
     valueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style:"currency",
-        currency:"USD"
-    }).format(convertedValue)
+      style: "currency",
+      currency: "USD",
+    }).format(inputValue / valueDolar);
+  }
+
+  if (select.value == "euro") {
+    // quando euro estiver selecionado
+    valueToConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputValue / valueEuro);
+  }
+
+  valueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputValue);
 }
 
-convertButton.addEventListener("click", convertValues)
+function changeCurrency() {
+  const currencyName = document.getElementById("nome-convertido") // nome da moeda
+  const currencyImg = document.querySelector(".bandeira-convertida") // bandeira
+  const valueToConverted = document.querySelector(".valor-convertido"); // valor convertido
+
+  if (select.value == "dolar") {
+    currencyName.innerHTML = "Dólar americano"
+    currencyImg.src = "./assets/dolar.png"
+    valueToConverted.innerHTML = "US$ 0,00" 
+  }
+
+  if (select.value == "euro") {
+    currencyName.innerHTML = "Euro"
+    currencyImg.src = "./assets/euro.png"
+    valueToConverted.innerHTML = "0,00 €"
+  }
+}
 
 
-
+select.addEventListener("change", changeCurrency);
+convertButton.addEventListener("click", convertValues);
