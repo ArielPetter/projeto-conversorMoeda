fc080d3d7e26bcf1af534a239b7fee23a17c9f58
ArielPetter <arielputzpetter@gmail.com>
2025-02-24 19:35:18 -0300

    add coins

--- script.js
+++ script.js
@@ -14,31 +14,58 @@ async function fetchExchangeRates() {
   );
   const libraValueRealTime = await libraResponse.json();
 
+  const btcResponse = await fetch(
+    "https://cointradermonitor.com/api/pbb/v1/ticker"
+  );
+
+  const btcValueRealTime = await btcResponse.json();
+
+  const ethResponse = await fetch(
+    "https://api.binance.com/api/v3/ticker/price?symbol=ETHBRL"
+  );
+
+  const ethValueRealTime = await ethResponse.json();
+
   return {
     dolar: coinsValueInRealTime.USDBRL.high,
     euro: coinsValueInRealTime.EURBRL.high,
     libra: 1 / libraValueRealTime.rates.GBP,
+    bitcoin: btcValueRealTime.last,
+    ethereum: ethValueRealTime.price,
   };
 }
 
+function getFormattedInputValue(fromCurrency) {
+  const input = document.querySelector(".main-input").value;
+
+  // Ajustar formato com base na moeda de origem
+  if (fromCurrency === "real") {
+    return parseFloat(input.replace(/\./g, "").replace(",", "."));
+  }
+
+  // Para dólar, euro, libra, btc e eth baseado em dol
+  return parseFloat(input.replace(/,/g, ""));
+}
+
 async function convertValues() {
-  const inputValue = parseFloat(document.querySelector(".main-input").value);
+  const inputValue = getFormattedInputValue(selectConverter.value);
   try {
     if (isNaN(inputValue) || inputValue <= 0) {
       Toastify({
         text: "Adicione um valor e tente novamente!",
-        duration: 2000, // Duração em milissegundos
-        close: false, // Exibe um botão de fechar
-        gravity: "top", // Posição do toast: "top" ou "bottom"
-        position: "left", // Posição do toast: "left", "center", "right"
-        backgroundColor: "rgba(138, 81, 252, 1)", // Cor de fundo (rgba)
-        color: "#00ff00", // Cor do texto (letras)
+        duration: 2000,
+        close: false,
+        gravity: "top",
+        position: "left",
+        backgroundColor: "rgba(138, 81, 252, 1)",
+        color: "#00ff00",
         className: "custom-toast",
       }).showToast();
       return;
     }
 
-    const { dolar, euro, libra } = await fetchExchangeRates();
+    const { dolar, euro, libra, bitcoin, ethereum } =
+      await fetchExchangeRates();
 
     const valueToConvert = document.querySelector(".valor-converter");
     const valueToConverted = document.querySelector(".valor-convertido");
@@ -51,7 +78,7 @@ async function convertValues() {
       inputValue,
       selectConverter.value,
       selectConverted.value,
-      { dolar, euro, libra }
+      { dolar, euro, libra, bitcoin, ethereum }
     );
 
     valueToConverted.innerHTML = formatCurrency(
@@ -82,6 +109,12 @@ function calculateConversion(inputValue, from, to, rates) {
     case "libra":
       valueInBRL = inputValue * rates.libra;
       break;
+    case "btc":
+      valueInBRL = inputValue * rates.bitcoin;
+      break;
+    case "eth":
+      valueInBRL = inputValue * rates.ethereum;
+      break;
     default:
       return inputValue;
   }
@@ -96,6 +129,10 @@ function calculateConversion(inputValue, from, to, rates) {
       return valueInBRL / rates.euro;
     case "libra":
       return valueInBRL / rates.libra;
+    case "btc":
+      return valueInBRL / rates.bitcoin;
+    case "eth":
+      return valueInBRL / rates.ethereum;
     default:
       return valueInBRL;
   }
@@ -107,9 +144,17 @@ function formatCurrency(value, currency) {
     dolar: { locale: "en-US", currency: "USD" },
     euro: { locale: "de-DE", currency: "EUR" },
     libra: { locale: "en-GB", currency: "GBP" },
+    btc: { locale: "en-US", currency: "BTC" },
+    eth: { locale: "en-US", currency: "ETH" },
   };
 
   const { locale, currency: curr } = currencyFormat[currency] || {};
+
+  // Exibe até 8 casas decimais para o caso do bitcoin e ethereum
+  if (currency === "btc" || currency == "eth") {
+    return value.toFixed(8);
+  }
+
   return new Intl.NumberFormat(locale, {
     style: "currency",
     currency: curr,
@@ -140,6 +185,14 @@ function changeCurrency(selectElement, isConverter = false) {
     case "libra":
       currencyName.innerHTML = "Libra";
       currencyImg.src = "./assets/libra.png";
+      break;
+    case "btc":
+      currencyName.innerHTML = "Bitcoin";
+      currencyImg.src = "./assets/btc.png";
+      break;
+    case "eth":
+      currencyName.innerHTML = "Ethereum";
+      currencyImg.src = "./assets/eth.png";
       break;
     default:
       console.error("Moeda não reconhecida");
